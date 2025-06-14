a8948d497d3186c94b7d76bae81524e77c70ca3a
rahadityaputra <rahadit.a.p123@gmail.com>
2025-06-14 16:44:30 +0700

    feat: create controller

---
+++ src/controllers/productController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+
+function getAllProducts(req: Request, res: Response) {
+
+}
+
+
+function getProductById(req: Request, res: Response) {
+
+}
+
+function createProduct(req: Request, res: Response) {
+
+}
+
+function updateProduct(req: Request, res: Response) {
+
+}
+
+function deleteProduct(req: Request, res: Response) {
+
+}
+export {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
+}
